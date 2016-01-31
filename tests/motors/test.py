b0190e39bdb4b869aed1677c890ebbaab0956b35
Parker Lusk <parkerclusk@gmail.com>
2016-01-31 11:36:07 +1030

    got bot spinning with t.spin(40000)

--- tests/motors/test.py
+++ tests/motors/test.py
@@ -3,12 +3,21 @@ import rcv3.roboclaw as r
 r.Open('/dev/ttySAC0', 38400)
 
 def readPIDQ():
-	pid
+	PIDQ = []
+	PIDQ.append(r.ReadM1VelocityPID(0x80))
+	PIDQ.append(r.ReadM2VelocityPID(0x80))
+	PIDQ.append(r.ReadM1VelocityPID(0x81))
+	return PIDQ
+
+def setAllPIDQ(p,i,d,q):
+	r.SetM1VelocityPID(0x80,p,i,d,q)
+	r.SetM2VelocityPID(0x80,p,i,d,q)
+	r.SetM1VelocityPID(0x81,p,i,d,q)
 
 def spin(speed):
 	r.SpeedM1(0x80, speed)
-	r.SpeedM2(0x80, speed)
-	r.SpeedM1(0x81, speed)
+	r.SpeedM2(0x80, -speed)
+#	r.SpeedM1(0x81, speed)
 
 def forward(speed):
 	r.SpeedM1(0x80, speed)
