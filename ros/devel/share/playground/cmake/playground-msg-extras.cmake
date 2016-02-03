1e91eb6509b713df2865285f9b81f356e46e04d9
Parker Lusk <parkerclusk@gmail.com>
2016-02-03 00:29:39 -0700

    integrated vision with ROS...and it worked

---
+++ ros/devel/share/playground/cmake/playground-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(playground_MESSAGE_FILES "/home/ecestudent/dev/lusk/ros/src/playground/msg/coords.msg")
+set(playground_SERVICE_FILES "")
---
+++ ros/devel/share/playground/cmake/playground-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
+
+set(playground_MSG_INCLUDE_DIRS "/home/ecestudent/dev/lusk/ros/src/playground/msg")
+set(playground_MSG_DEPENDENCIES std_msgs)
