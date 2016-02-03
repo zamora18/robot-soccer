08448ff0d845d818c8ef8ca207cc316b77255246
Parker Lusk <parkerclusk@gmail.com>
2016-02-03 10:21:44 -0700

    added terrible, terrible code to do something with the skillz competition

--- ros/src/playground/scripts/listener.py
+++ ros/src/playground/scripts/listener.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+# Delete this file
+
 import rospy
 from playground.msg import coords
 
@@ -8,6 +10,10 @@ import velchange as v
 _x = 0
 _y = 0
 _theta = 0
+
+
+
+_state = 'SPIN' # 'SQUARE', 'HOME'
 
 def callback(data):
     #rospy.loginfo(rospy.get_caller_id() + "I heard (%s,%s,%s)", data.robot_x,data.robot_y,data.robot_theta)
@@ -37,28 +43,88 @@ def listener():
 
     rospy.Subscriber("vision", coords, callback)
 
+    counter = 0
+
     rate = rospy.Rate(100) # 100hz
     while not rospy.is_shutdown():
 
-        if not _isClose(_theta, 0):
-            v.goXYOmega(0, 0, 2.5)
-    
-        elif not _isClose(_x, 0) or not _isClose(_y, 0):
-            print "going to 0 theta"
 
-            sign_x = -1 if _x<0 else 1
-            sign_y = -1 if _y<0 else 1
+        counter = counter+1
 
-            vx = 1 if abs(_x)>abs(_y) else abs(_x)/abs(_y)
-            vy = 1 if abs(_y)>abs(_x) else abs(_y)/abs(_x)
+        if _state == 'SPIN':
+            v.goXYOmega(0,0,7)
 
-            vx = -1*sign_x*vx
-            vy = -1*sign_y*vy
+            if counter == 300:
+                counter = 0
+                _state = 'SLEEP1'
 
-            v.goXYOmega(vx, vy, 0)
+        elif _state == 'SLEEP1':
+            if counter == 200:
+                counter = 0
+                _state = 'SQUARE1'
 
-        else:
-            v.goXYOmega(0, 0, 0)
+
+
+
+        # ---------------------------
+        # Sqauares
+
+
+        elif _state == 'SQUARE1':
+            velchange.goXYOmega(.5,0,0)
+
+            if counter == 100:
+                counter = 0
+                _state = 'SQUARE2'
+
+        elif _state == 'SQUARE2':
+            velchange.goXYOmega(0,.5,0)
+
+            if counter == 100:
+                counter = 0
+                _state = 'SQUARE3'
+
+        elif _state == 'SQUARE3':
+            velchange.goXYOmega(-0.5,0,0)
+
+            if counter == 100:
+                counter = 0
+                _state = 'SQUARE4'
+
+        elif _state == 'SQUARE4':
+            velchange.goXYOmega(0,-.5,0)
+
+            if counter == 100:
+                counter = 0
+                _state = 'SLEEP2'
+
+        # ---------------------------
+
+        elif _state == 'SLEEP2':
+            if counter == 200:
+                counter = 0
+                _state = 'HOME'
+            
+        elif _state == 'HOME':
+            if not _isClose(_theta, 0):
+                print "going to 0 theta"
+                v.goXYOmega(0, 0, 2.5)
+        
+            elif not _isClose(_x, 0) or not _isClose(_y, 0):
+                print "going home"
+                sign_x = -1 if _x<0 else 1
+                sign_y = -1 if _y<0 else 1
+
+                vx = 1 if abs(_x)>abs(_y) else abs(_x)/abs(_y)
+                vy = 1 if abs(_y)>abs(_x) else abs(_y)/abs(_x)
+
+                vx = -1*sign_x*vx
+                vy = -1*sign_y*vy
+
+                v.goXYOmega(vx, vy, 0)
+
+            else:
+                v.goXYOmega(0, 0, 0)
             
         rate.sleep()
 
