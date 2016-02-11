156d6d846a0846ee615a3dcf4074a629df13a23d
Parker Lusk <parkerclusk@gmail.com>
2016-02-12 08:42:08 +1030

    uhh

--- tests/motors/motion.py
+++ tests/motors/motion.py
@@ -23,6 +23,22 @@ def drive(vx,vy,omega):
 
     return (s1, s2, s3)
 
+def drive_smooth(vx,vy,omega):
+    # Convert from world to robot wheel speeds
+    (OMEGA1, OMEGA2, OMEGA3) = p.world_to_wheel_speeds(vx, vy, omega, _theta)
+
+    # Convert rad/s to qpps
+    s1 = int(OMEGA1*PULSES_PER_RADIAN)
+    s2 = int(OMEGA2*PULSES_PER_RADIAN)
+    s3 = int(OMEGA3*PULSES_PER_RADIAN)
+
+    # Drive the motors
+    w.SpeedAccel(w.M1, s1*2, s1)
+    w.SpeedAccel(w.M2, s2*2, s2)
+    w.SpeedAccel(w.M3, s3*2, s3)
+
+    return (s1, s2, s3)
+
 def get_velocities():
     # Ask RoboClaw for encoder speed (not raw speed)
     (s1, s2, s3) = (w.ReadSpeed(w.M1)[1], w.ReadSpeed(w.M2)[1], w.ReadSpeed(w.M3)[1])
@@ -41,9 +57,9 @@ def stop():
     drive(0,0,0)
 
 def smooth_stop():
-    w.SpeedAccel(w.M1,1,w.QPPS.M1*2)
-    w.SpeedAccel(w.M2,1,w.QPPS.M2*2)
-    w.SpeedAccel(w.M3,1,w.QPPS.M3*2)
+    w.SpeedAccel(w.M1,w.QPPS.M1*1,1)
+    w.SpeedAccel(w.M2,w.QPPS.M2*1,1)
+    w.SpeedAccel(w.M3,w.QPPS.M3*1,1)
 
 def kill():
     """Kill
