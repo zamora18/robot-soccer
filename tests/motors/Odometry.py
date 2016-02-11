048dfb66132178d5d053ec4907ba40dde130be44
Parker Lusk <parkerclusk@gmail.com>
2016-02-11 16:52:54 -0700

    trying to add data spike stuff

--- tests/motors/Odometry.py
+++ tests/motors/Odometry.py
@@ -10,14 +10,38 @@ x = 0
 y = 0
 theta = 0
 
-#def init(x=0,y=0,theta=0):
-#    globals()['x'] = x
-#    globals()['y'] = y
-#    globals()['theta'] = theta
+# Keep track of what the previous s1, s2, s3 where
+_prev = { 's1': 0, 's2': 0, 's3': 0 }
+_data_spikes = { 's1': 0, 's2': 0, 's3': 0 }
+_data_spike_tolerance = 1000
 
-def init():
-    global x,y,theta
-    x = y = theta = 0
+def _is_data_spike(s1, s2, s3):
+    diff1 = abs(s1 - _prev['s1'])
+    diff2 = abs(s2 - _prev['s2'])
+    diff3 = abs(s3 - _prev['s3'])
+
+    if diff1 > _data_spike_tolerance:
+        # s1 had a spike
+        _data_spikes['s1'] += 1
+        return True
+
+    if diff2 > _data_spike_tolerance:
+        # s2 had a spike
+        _data_spikes['s2'] += 1
+        return True
+
+    if diff3 > _data_spike_tolerance:
+        # s3 had a spike
+        _data_spikes['s3'] += 1
+        return True
+
+    return False
+
+
+def init(x=0,y=0,theta=0):
+   globals()['x'] = x
+   globals()['y'] = y
+   globals()['theta'] = theta
 
 
 def update(time_since_last_update):
@@ -28,6 +52,10 @@ def update(time_since_last_update):
 
     # Get the current world velocities based on current motor speeds
     (vx, vy, omega, s1, s2, s3) = motion.get_velocities()
+
+    # Check if there was a data spike
+    if _is_data_spike(s1, s2, s3):
+        print _data_spikes
 
     # Put data matrix form for easy manipulation
     V = np.matrix([ [vx],[vy],[omega] ])
@@ -41,7 +69,7 @@ def update(time_since_last_update):
     y = P_next.getA()[1][0]
     theta = P_next.getA()[2][0]
 
-    return (x, y, theta, s1,s2,s3)
+    return (x, y, theta, s1, s2, s3)
 
 def RK4():
     """Runge-Kutta 4
