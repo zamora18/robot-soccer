db99e05445842da6c4787bfa49f08810db35c58c
Parker Lusk <parkerclusk@gmail.com>
2016-02-11 19:46:24 -0700

    trying to use theta in odom

--- tests/motors/motion.py
+++ tests/motors/motion.py
@@ -1,15 +1,15 @@
-import wheelbase as w
-import param as p
 import numpy as np
 
-_theta = 0 # same as heading
+import wheelbase as w
+import param as p
+import Odometry
 
 PULSES_PER_REV = 19822
 PULSES_PER_RADIAN = (PULSES_PER_REV / (2.0*np.pi))
 
 def drive(vx,vy,omega,smooth=False):
     # Convert from world to robot wheel speeds
-    (OMEGA1, OMEGA2, OMEGA3) = p.world_to_wheel_speeds(vx, vy, omega, _theta)
+    (OMEGA1, OMEGA2, OMEGA3) = p.world_to_wheel_speeds(vx, vy, omega, Odometry.theta)
 
     # Convert rad/s to qpps
     s1 = int(OMEGA1*PULSES_PER_RADIAN)
@@ -41,7 +41,7 @@ def get_velocities():
     OMEGA3 = s3/PULSES_PER_RADIAN
 
     # Convert from wheel angular speeds to world frame velocities
-    (vx, vy, omega) = p.wheel_speeds_to_world(OMEGA1, OMEGA2, OMEGA3, _theta)
+    (vx, vy, omega) = p.wheel_speeds_to_world(OMEGA1, OMEGA2, OMEGA3, Odometry.theta)
 
     return (vx, vy, omega, s1, s2, s3)
 
