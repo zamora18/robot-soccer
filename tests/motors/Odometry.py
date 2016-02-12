a86c08e6974493b3d8adacbd8cf2d1ff50598093
Parker Lusk <parkerclusk@gmail.com>
2016-02-11 17:11:27 -0700

    spikes

--- tests/motors/Odometry.py
+++ tests/motors/Odometry.py
@@ -13,12 +13,17 @@ theta = 0
 # Keep track of what the previous s1, s2, s3 where
 _prev = { 's1': 0, 's2': 0, 's3': 0 }
 _data_spikes = { 's1': 0, 's2': 0, 's3': 0 }
-_data_spike_tolerance = 10000
+_data_spike_tolerance = 65536
 
 def _is_data_spike(s1, s2, s3):
     diff1 = abs(s1 - _prev['s1'])
     diff2 = abs(s2 - _prev['s2'])
     diff3 = abs(s3 - _prev['s3'])
+
+    # Update for next time
+    _prev['s1'] = s1
+    _prev['s2'] = s2
+    _prev['s3'] = s3
 
     if diff1 > _data_spike_tolerance:
         # s1 had a spike
@@ -34,11 +39,6 @@ def _is_data_spike(s1, s2, s3):
         # s3 had a spike
         _data_spikes['s3'] += 1
         return True
-
-    # No spike, so update for next time
-    _prev['s1'] = s1
-    _prev['s2'] = s2
-    _prev['s3'] = s3
 
     return False
 
