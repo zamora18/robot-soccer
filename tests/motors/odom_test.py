7082e86ad09eeec7ba24f641633b1aaa8969db46
Parker Lusk <parkerclusk@gmail.com>
2016-02-11 18:25:04 -0700

    code clean up

--- tests/motors/odom_test.py
+++ tests/motors/odom_test.py
@@ -1,54 +1,13 @@
-import select
-import signal
-import sys
-import termios
 import time
-import tty
+import sys
 
 import numpy as np
+from Getch import _Getch
 
 from repeated_timer import RepeatedTimer
 import motion
 import wheelbase as w
 import Odometry
-
-class _Getch:
-    """Gets a single character from standard input.  Does not echo to the
-screen."""
-    def __init__(self):
-        try:
-            self.impl = _GetchWindows()
-        except ImportError:
-            self.impl = _GetchUnix()
-
-    def __call__(self): return self.impl()
-
-
-class _GetchUnix:
-    def __init__(self):
-        import tty, sys
-
-    def __call__(self):
-        import sys, tty, termios
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
-        try:
-            tty.setraw(sys.stdin.fileno())
-            ch = sys.stdin.read(1)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        return ch
-
-
-class _GetchWindows:
-    def __init__(self):
-        import msvcrt
-
-    def __call__(self):
-        import msvcrt
-        return msvcrt.getch()
-
-
 
 _motion_timer = None
 _odom_timer = None
