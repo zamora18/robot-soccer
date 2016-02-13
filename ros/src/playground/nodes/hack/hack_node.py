6d7dfd4d9f08fa264993b9fed29ab66d26dfd3e4
Parker Lusk <parkerclusk@gmail.com>
2016-02-13 16:53:04 -0700

    added hack node for ball tracking

---
+++ ros/src/playground/nodes/hack/hack_node.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import roslib; roslib.load_manifest('playground')
+import rospy
+from geometry_msgs.msg import Pose2D
+from playground.msg import coords
+
+import numpy as np
+
+import Controller
+
+_hack_period = 1.0/100
+
+_ball_x = 0
+_ball_y = 0
+
+def _handle_vision_coords(msg):
+    # rospy.loginfo(rospy.get_caller_id() + "I heard (%s,%s,%s)", data.linear.x,data.linear.y,data.angular.z)
+    global _ball_x, _ball_y
+    _ball_x = msg.ball_x
+    _ball_y = msg.ball_y
+
+def main():
+    rospy.init_node('hack', anonymous=False)
+
+    rospy.Subscriber('vision', coords, _handle_vision_coords)
+    pub = rospy.Publisher('desired_position', Pose2D, queue_size=10)
+
+    rate = rospy.Rate(int(1/_hack_period))
+    while not rospy.is_shutdown():
+
+        msg = Pose2D()
+        msg.x = _ball_x
+        msg.y = _ball_y
+        msg.z = 0
+
+        pub.publish(msg)
+
+        rate.sleep()
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+if __name__ == '__main__':
+    main()
