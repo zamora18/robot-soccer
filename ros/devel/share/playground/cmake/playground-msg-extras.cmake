357bf457e3e9e2a467acec81dd41e917031a3bb1
Parker Lusk <parkerclusk@gmail.com>
2016-02-13 15:57:34 -0700

    removed devel folder from ROS

--- ros/devel/share/playground/cmake/playground-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(playground_MESSAGE_FILES "/root/dev/lusk/repo/ros/src/playground/msg/coords.msg;/root/dev/lusk/repo/ros/src/playground/msg/EncoderEstimates.msg;/root/dev/lusk/repo/ros/src/playground/msg/QPPS.msg;/root/dev/lusk/repo/ros/src/playground/msg/WorldVelocities.msg")
-set(playground_SERVICE_FILES "")
