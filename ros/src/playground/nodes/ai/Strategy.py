12b4092a7a125d3ca2b880abe73890f09565fc41
Jake Zamora <zamora18@gmail.com>
2016-02-15 15:24:22 -0700

    added ai node and 'strategy' file

---
+++ ros/src/playground/nodes/ai/Strategy.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def choose_strategy(robot, ball):
+    return (0.5, 0.5, 0) # x_c and y_c are in meters. theta in degrees
+
+def _strong_offense(robot, ball):
+    # for now we want to make one robot kick the ball into the open goal
+    pass
+
+def _strong_defense(robot, ball):
+    #for not we want to make one robot defend the goal
+    pass
