5a953a8eee0740eeb40edc3599d6db551c13b01d
Parker Lusk <parkerclusk@gmail.com>
2016-02-23 10:18:31 -0700

    gui changes

--- gui/main.m
+++ gui/main.m
@@ -55,30 +55,29 @@ function main_OpeningFcn(hObject, eventdata, handles, varargin)
 % Choose default command line output for main
 handles.output = hObject;
 
-
 % Setup Position Plot
 handles.plot_position = plot(handles.fig_position,0,0);
 set(handles.fig_position,'XLim',[-3 3],'YLim',[-2 2]);
-daspect([1 1 1]);
+daspect(handles.fig_position, [1 1 1]);
 xlabel(handles.fig_position, 'width (meters)');
 ylabel(handles.fig_position, 'height (meters)');
 set(handles.fig_position, 'XGrid', 'on', 'YGrid', 'on');
 
-
 % Setup Velocity Plot
 handles.plot_velocity = quiver(handles.fig_velocity,0,0,0,0,0);
-xlim([-2.5 2.5]); ylim([-1.5 1.5]);
-daspect([1 1 1]);
-xlabel('width (m/s)');
-ylabel('height (m/s)');
-grid on;
-% set(handles.plot.Parent,'XLim',[-2 2],'YLim',[-2 2]);
+set(handles.fig_velocity,'XLim',[-2.5 2.5],'YLim',[-1.5 1.5]);
+daspect(handles.fig_velocity, [1 1 1]);
+xlabel(handles.fig_velocity,'width (m/s)');
+ylabel(handles.fig_velocity,'height (m/s)');
+set(handles.fig_velocity, 'XGrid', 'on', 'YGrid', 'on');
+
+% Setup Desired Position Table
+set(handles.table_desired_position,'Data', {0 0 0});
 
 % Setup ROS Subscribers
 handles.sub.vision_robot_position = rossubscriber('/vision_robot_position', 'geometry_msgs/Pose2D', {@visionRobotPositionCallback,handles});
 handles.sub.desired_position = rossubscriber('/desired_position', 'geometry_msgs/Pose2D', {@desiredPositionCallback,handles});
 handles.sub.vel_cmds = rossubscriber('/vel_cmds', 'geometry_msgs/Twist', {@velCmdsCallback,handles});
-
 
 % And Publishers
 handles.pub.desired_position = rospublisher('/desired_position', 'geometry_msgs/Pose2D');
@@ -106,39 +105,33 @@ function figure1_CloseRequestFcn(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
+clear handles.sub
+clear handles.pub
 
-
-% if isequal(get(hObject, 'waitstatus'), 'waiting')
-%     % The GUI is still in UIWAIT, us UIRESUME
-%     uiresume(hObject);
-% else
-    clear handles.sub
-
-    % The GUI is no longer waiting, just close it
-    delete(hObject);
-% end
+% The GUI is no longer waiting, just close it
+delete(hObject);
 
 function visionRobotPositionCallback(src, msg, handles)   
-    x = get(handles.plot_position,'XData');
-    y = get(handles.plot_position,'YData');
-    x = [x msg.X/100];
-    y = [y msg.Y/100];
-    set(handles.plot_position,'XData',x,'YData',y);
-    
-    set(handles.table_position,'Data', {msg.X/100 msg.Y/100 msg.Theta});
+x = get(handles.plot_position,'XData');
+y = get(handles.plot_position,'YData');
+x = [x msg.X/100];
+y = [y msg.Y/100];
+set(handles.plot_position,'XData',x,'YData',y);
+
+set(handles.table_position,'Data', {msg.X/100 msg.Y/100 msg.Theta});
     
 function desiredPositionCallback(src, msg, handles)
-    set(handles.table_desired_position,'Data', {msg.X msg.Y msg.Theta});
+set(handles.table_desired_position,'Data', {msg.X msg.Y msg.Theta});
     
 function velCmdsCallback(src, msg, handles)
-    
-    vx = msg.Linear.X;
-    vy = msg.Linear.Y;
-    w  = msg.Angular.Z;
 
-    set(handles.plot_velocity,'XData',0,'YData',0,'UData',vx,'VData',vy);
-    
-    set(handles.table_velocity,'Data', {vx vy w});
+vx = msg.Linear.X;
+vy = msg.Linear.Y;
+w  = msg.Angular.Z;
+
+set(handles.plot_velocity,'XData',0,'YData',0,'UData',vx,'VData',vy);
+
+set(handles.table_velocity,'Data', {vx vy w});
 
 
 % --- Executes on button press in btn_clear_position.
@@ -147,7 +140,7 @@ function btn_clear_position_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
-    set(handles.plot_position,'XData',0,'YData',0)
+set(handles.plot_position,'XData',0,'YData',0)
 
 
 % --- Executes on button press in btn_set_desired_position.
@@ -155,14 +148,14 @@ function btn_set_desired_position_Callback(hObject, eventdata, handles)
 % hObject    handle to btn_set_desired_position (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-    global c_raw
-    global desired
-    c_raw = get(handles.table_desired_position,'Data');
-    c = c_raw(1,:)
-    desired = cellfun(@str2num,c);
-    
-    msg = rosmessage(handles.pub.desired_position);
-    msg.X = desired(1);
-    msg.Y = desired(2);
-    msg.Theta = desired(3);
-    send(handles.pub.desired_position, msg);
+global c_raw
+global desired
+c_raw = get(handles.table_desired_position,'Data');
+c = c_raw(1,:)
+desired = cellfun(@str2num,c);
+
+msg = rosmessage(handles.pub.desired_position);
+msg.X = desired(1);
+msg.Y = desired(2);
+msg.Theta = desired(3);
+send(handles.pub.desired_position, msg);
