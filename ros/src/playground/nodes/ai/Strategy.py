4cb4f00ccd517516fec0eefcb580c3aa317a315f
Jake Zamora <zamora18@gmail.com>
2016-02-23 22:30:21 -0700

    I'm getting tired

--- ros/src/playground/nodes/ai/Strategy.py
+++ ros/src/playground/nodes/ai/Strategy.py
@@ -21,38 +21,38 @@ def _strong_offense(robot, ball):
     # for now we want to make one robot kick the ball into the open goal
     #
     # arctan2([y], [x])
-    theta_ball_to_goal = np.arctan2([ ball['yhat'] - _goal_position_opp[1] ], [ _goal_position_opp[0] - ball['xhat'] ])
-    theta_ball_to_goal = theta_ball_to_goal*180/np.pi
-    theta_bot_to_goal = np.arctan2([ robot['yhat'] - _goal_position_opp[1] ], [ _goal_position_opp[0] - robot['xhat'] ])
-    theta_bot_to_goal = theta_bot_to_goal*180/np.pi
-    dist_from_ball = _get_distance(robot, ball)
+    theta_ball_to_goal      = np.arctan2([ ball['yhat'] - _goal_position_opp[1] ], [ _goal_position_opp[0] - ball['xhat'] ])
+    theta_ball_to_goal_deg  = theta_ball_to_goal*180/np.pi
+    theta_bot_to_goal       = np.arctan2([ robot['yhat'] - _goal_position_opp[1] ], [ _goal_position_opp[0] - robot['xhat'] ])
+    theta_bot_to_goal_deg   = theta_bot_to_goal*180/np.pi
+    dist_from_ball          = _get_distance(robot, ball)
 
     if (ball['xhat'] > _goal_position_opp[0]): #might have to tweak this a little bit
         #ball is in goal, don't move robot anywhere
         return (robot['xhat'], robot['yhat'], robot['thetahat'])
     else:
         # if robot is behind the ball and aligned towards goal
-        if (theta_ball_to_goal == robot['thetahat'] and 
-            theta_bot_to_goal == theta_ball_to_goal and 
-            dist_from_ball <= _dist_behind_ball+_robot_half_width): #taking into account 1/2 robot width
-            #kick ball towards goal 6 inches
-            x_c = ball['xhat'] + (_dist_behind_ball+_kick_dist)*np.cos(theta_ball_to_goal)
-            y_c = ball['yhat'] + (_dist_behind_ball+_kick_dist)*np.sin(theta_ball_to_goal)
-            return (x_c, y_c, theta_ball_to_goal)
-        else: 
+        #if (theta_ball_to_goal_deg == robot['thetahat'] and \
+         #   theta_bot_to_goal_deg == theta_ball_to_goal_deg and \
+          #  dist_from_ball <= (_dist_behind_ball+_robot_half_width)): #taking into account 1/2 robot width
+          #  #kick ball towards goal 6 inches
+          #  x_c = ball['xhat'] + (_dist_behind_ball+_kick_dist)*np.cos(theta_ball_to_goal)
+          #  y_c = ball['yhat'] + (_dist_behind_ball+_kick_dist)*np.sin(theta_ball_to_goal)
+          #  return (x_c, y_c, theta_ball_to_goal_deg)
+       # else: 
             # get aligned with ball facing goal
             x_c = ball['xhat'] - (_dist_behind_ball+_robot_half_width)*np.cos(theta_ball_to_goal)
             y_c = ball['yhat'] - (_dist_behind_ball+_robot_half_width)*np.sin(theta_ball_to_goal)
-            return (x_c, y_c, theta_ball_to_goal)
+            return (x_c, y_c, theta_ball_to_goal_deg)
             
 
 def _strong_defense(robot, ball):
     #for not we want to make one robot defend the goal
     theta_c = np.arctan2([ _goal_position_home[1] + ball['yhat'] ], [ ball['xhat'] - _goal_position_opp[0] ])
-    theta_c = theta_c*180/np.pi
+    theta_c_deg = theta_c*180/np.pi
     x_c = _goal_position_home[0] + (_goal_box_length+_robot_half_width)*np.cos(theta_c)
     y_c = _goal_position_home[1] + (_goal_box_length+_robot_half_width)*np.sin(theta_c)
-    return (x_c, y_c, theta_c)
+    return (x_c, y_c, theta_c_deg)
     
 def _get_distance(object_1, object_2):
     x_dist = object_1['xhat'] - object_2['xhat']
