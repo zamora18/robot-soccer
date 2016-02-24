1adeb0f580799a78a28f01a824f84daf3eb9e022
Jake Zamora <zamora18@gmail.com>
2016-02-23 21:16:07 -0700

    Updated Strategy.py, improved ball approach)

--- ros/src/playground/nodes/ai/Strategy.py
+++ ros/src/playground/nodes/ai/Strategy.py
@@ -4,6 +4,7 @@ import numpy as np
 _field_length       = 3.048 # meters (10ft)
 _field_width        = 1.52  # meters (5 ft)
 _robot_width        = 0.1841 # meters (7.25in)
+_robot_half_width   = _robot_width/2
 _goal_width         = _field_width/3
 _goal_box_length    = _field_length/10
 _goal_position_home = [-_field_length, 0, 0] #this could change depending on camera
@@ -29,15 +30,15 @@ def _strong_offense(robot, ball):
         return (robot['xhat'], robot['yhat'], robot['thetahat'])
     else:
         # if robot is behind the ball and aligned towards goal
-        if (theta_ball_to_goal == robot['thetahat'] and dist_from_ball <= _dist_behind_ball+_robot_width/2): #taking into account 1/2 robot width
+        if (theta_ball_to_goal == robot['thetahat'] and dist_from_ball <= _dist_behind_ball+_robot_half_width): #taking into account 1/2 robot width
             #kick ball towards goal 6 inches
-            x_c = ball['xhat'] + (_dist_behind_ball + _kick_dist)*np.cos(theta)
-            y_c = ball['yhat'] + (_dist_behind_ball + _kick_dist)*np.sin(theta)
+            x_c = ball['xhat'] + (_dist_behind_ball+_kick_dist)*np.cos(theta)
+            y_c = ball['yhat'] + (_dist_behind_ball+_kick_dist)*np.sin(theta)
             return (x_c, y_c, theta_ball_to_goal)
         else: 
             # get aligned with ball facing goal
-            x_c = ball['xhat'] - (_dist_behind_ball+_robot_width/2)*np.cos(theta)
-            y_c = ball['yhat'] - (_dist_behind_ball+_robot_width/2)*np.sin(theta)
+            x_c = ball['xhat'] - (_dist_behind_ball+_robot_half_width)*np.cos(theta)
+            y_c = ball['yhat'] - (_dist_behind_ball+_robot_half_width)*np.sin(theta)
             return (x_c, y_c, theta_ball_to_goal)
             
 
@@ -45,8 +46,8 @@ def _strong_defense(robot, ball):
     #for not we want to make one robot defend the goal
     theta_c = np.arctan2([ _goal_position_home[1] + ball['yhat'] ], [ ball['xhat'] - _goal_position_opp[0] ])
     theta_c = theta_c*180/np.pi
-    x_c = _goal_position_home[0] + _goal_box_length*np.cos(theta)
-    y_c = _goal_position_home[1] + _goal_box_length*np.sin(theta)
+    x_c = _goal_position_home[0] + (_goal_box_length+_robot_half_width)*np.cos(theta)
+    y_c = _goal_position_home[1] + (_goal_box_length+_robot_half_width)*np.sin(theta)
     return (x_c, y_c, theta_c)
     
 def _get_distance(object_1, object_2):
@@ -54,5 +55,10 @@ def _get_distance(object_1, object_2):
     y_dist = object_1['yhat'] - object_2['yhat']
     distance = np.sqrt(x_dist**2 + y_dist**2)
     return distance
+
+
+
+
+
 
 
