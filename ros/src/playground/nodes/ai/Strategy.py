2ca91f848738e480e8a7ab306adfe176b0843932
Jake Zamora <zamora18@gmail.com>
2016-02-23 23:56:08 -0700

    Almost ready for competition

--- ros/src/playground/nodes/ai/Strategy.py
+++ ros/src/playground/nodes/ai/Strategy.py
@@ -33,15 +33,15 @@ def _strong_offense(robot, ball):
         return (robot['xhat'], robot['yhat'], robot['thetahat'])
     else:
         # if robot is behind the ball and aligned towards goal
-        #if (theta_ball_to_goal_deg == robot['thetahat'] and \
-         #   theta_bot_to_goal_deg == theta_ball_to_goal_deg and \
-          #  dist_from_ball <= (_dist_behind_ball+_robot_half_width)): #taking into account 1/2 robot width
-          #  #kick ball towards goal 6 inches
-          #  x_c = ball['xhat'] + (_dist_behind_ball+_kick_dist)*np.cos(theta_ball_to_goal)
-          #  y_c = ball['yhat'] + (_dist_behind_ball+_kick_dist)*np.sin(theta_ball_to_goal)
-          #  return (x_c, y_c, theta_ball_to_goal_deg)
-       # else: 
-            # get aligned with ball facing goal
+        if (theta_ball_to_goal_deg == robot['thetahat'] and \
+            theta_bot_to_goal_deg == theta_ball_to_goal_deg and \
+            dist_from_ball <= (_dist_behind_ball+_robot_half_width)): #taking into account 1/2 robot width
+            #kick ball towards goal 6 inches
+            x_c = ball['xhat'] + (_dist_behind_ball+_kick_dist)*np.cos(theta_ball_to_goal)
+            y_c = ball['yhat'] + (_dist_behind_ball+_kick_dist)*np.sin(theta_ball_to_goal)
+            return (x_c, y_c, theta_ball_to_goal_deg)
+        else: 
+            get aligned with ball facing goal
             x_c = ball['xhat'] - (_dist_behind_ball+_robot_half_width)*np.cos(theta_ball_to_goal)
             y_c = ball['yhat'] - (_dist_behind_ball+_robot_half_width)*np.sin(theta_ball_to_goal)
             return (x_c, y_c, theta_ball_to_goal_deg)
@@ -51,7 +51,7 @@ def _strong_defense(robot, ball):
     #for now we want to make one robot defend the goal
     theta_c = np.arctan2([ _goal_position_home[1] + ball['yhat'] ], [ ball['xhat'] - _goal_position_opp[0] ])
     theta_c_deg = theta_c*180/np.pi
-    x_c = _goal_position_home[0] + _goal_box_length + _robot_half_width - 0 #_goal_position_home[0] + (_goal_box_length+_robot_half_width)*np.cos(theta_c)
+    x_c = _goal_position_home[0] + _goal_box_length + _robot_half_width #_goal_position_home[0] + (_goal_box_length+_robot_half_width)*np.cos(theta_c)
     
     if (ball['yhat'] > _goal_box_width/2):
         y_c = _goal_box_width/2
