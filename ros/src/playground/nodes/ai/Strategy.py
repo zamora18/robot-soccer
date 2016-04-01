d383f0197f8d60a72ed6da693750dd190081d326
Jake Zamora <zamora18@gmail.com>
2016-04-01 11:35:04 -0600

    Ready to test 2v2 code

--- ros/src/playground/nodes/ai/Strategy.py
+++ ros/src/playground/nodes/ai/Strategy.py
@@ -31,7 +31,7 @@ def choose_strategy(me, my_teammate, opponent1, opponent2, ball, goal, one_v_one
     # update_opponents_strategy_variables(opponent1, opponent2, ball)
     # Check to see if someone scored a goal
     
-    one_v_one = True
+    one_v_one = False
 
     #for now, we will just focus on aggressive offense
     # if goal:
